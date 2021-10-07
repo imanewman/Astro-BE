8d5d40216bcd293c9e5a716a8567d5ea5aa82ec6
Tim <tjnewman111@gmail.com>
2021-10-06 19:03:26 -0700

    added secondary house tests

--- test/test_houses.py
+++ test/test_houses.py
@@ -1,17 +1,18 @@
 from astro.chart.point import create_points_with_attributes
 from astro.chart import calculate_whole_sign_house_cusps, calculate_whole_sign_house_of_point, \
-    calculate_traditional_house_rulers, calculate_houses
-from astro.util import ZodiacSign, Point
+    calculate_traditional_house_rulers, calculate_whole_sign_houses, calculate_secondary_houses, \
+    calculate_secondary_house_cusps, calculate_secondary_house_of_point
+from astro.util import ZodiacSign, Point, HouseSystem
 from astro.util.tim import tim_natal
 
 
-def test_calculate_houses():
+def test_calculate_whole_sign_houses():
     """
     Tests that all the information about house placements is added.
     """
 
     points = create_points_with_attributes(tim_natal)
-    houses_whole_sign = calculate_houses(points)[0]
+    houses_whole_sign = calculate_whole_sign_houses(points)
 
     assert houses_whole_sign[0].number is 1
     assert houses_whole_sign[0].sign == ZodiacSign.sagittarius
@@ -21,7 +22,7 @@ def test_calculate_houses():
     assert points[Point.venus].houses_whole_sign.ruled_houses == [6, 11]
 
 
-def test_calculate_whole_sign_houses():
+def test_calculate_whole_sign_house_cusps():
     """
     Tests that the houses are properly calculated from the ascendant.
     """
@@ -54,7 +55,7 @@ def test_calculate_whole_sign_house_of_point():
     assert house_placements[10].points == [Point.mercury]
 
 
-def test_calculate_traditional_house_rulers():
+def test_calculate_whole_sign_house_rulers():
     """
     Tests that the houses each planet rules are properly calculated.
     """
@@ -69,3 +70,79 @@ def test_calculate_traditional_house_rulers():
 
     assert mercury.houses_whole_sign.ruled_houses == [7, 10]
     assert jupiter.houses_whole_sign.ruled_houses == [1, 4]
+
+
+def test_calculate_secondary_houses():
+    """
+    Tests that all the information about house placements is added.
+    """
+
+    points = create_points_with_attributes(tim_natal)
+    houses_secondary = calculate_secondary_houses(points, tim_natal.event, HouseSystem.porphyry)
+
+    assert houses_secondary[0].number is 1
+    assert houses_secondary[0].sign == ZodiacSign.sagittarius
+    assert houses_secondary[0].points == [Point.ascendant, Point.mars]
+
+    assert points[Point.venus].houses_secondary.house is 12
+    assert points[Point.venus].houses_secondary.ruled_houses == [6, 11]
+
+
+def test_calculate_secondary_house_cusps():
+    """
+    Tests that the houses are properly calculated from the ascendant.
+    """
+
+    house_placements = calculate_secondary_house_cusps(tim_natal.event, HouseSystem.porphyry)
+
+    assert house_placements[0].number is 1
+    assert house_placements[0].sign == ZodiacSign.sagittarius
+    assert house_placements[11].number is 12
+    assert house_placements[11].sign == ZodiacSign.scorpio
+
+
+def test_calculate_secondary_house_of_point__normal():
+    """
+    Tests that the houses a planet falls in are properly calculated.
+    """
+
+    points = create_points_with_attributes(tim_natal)
+    venus = points[Point.venus]
+    house_placements = calculate_secondary_house_cusps(tim_natal.event, HouseSystem.porphyry)
+
+    calculate_secondary_house_of_point(venus, house_placements)
+
+    assert venus.houses_secondary.house is 12
+    assert house_placements[11].points == [Point.venus]
+
+
+def test_calculate_secondary_house_of_point__wrapped():
+    """
+    Tests that the houses a planet falls in are properly calculated
+    when the following house cusp wraps past 0 degrees longitude.
+    """
+
+    points = create_points_with_attributes(tim_natal)
+    inner_heaven = points[Point.inner_heaven]
+    house_placements = calculate_secondary_house_cusps(tim_natal.event, HouseSystem.porphyry)
+
+    calculate_secondary_house_of_point(inner_heaven, house_placements)
+
+    assert inner_heaven.houses_secondary.house is 4
+    assert house_placements[3].points == [Point.inner_heaven]
+
+
+def test_calculate_secondary_house_rulers():
+    """
+    Tests that the houses each planet rules are properly calculated.
+    """
+
+    points = create_points_with_attributes(tim_natal)
+    mercury = points[Point.mercury]
+    jupiter = points[Point.jupiter]
+    house_placements = calculate_secondary_house_cusps(tim_natal.event, HouseSystem.porphyry)
+
+    calculate_traditional_house_rulers(points, house_placements, False)
+
+    assert mercury.houses_secondary.ruled_houses == [7, 10]
+    assert jupiter.houses_secondary.ruled_houses == [1, 4]
